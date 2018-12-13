a2480145e3a36fa213d9d968e36cb8c3fde1a866
Stewart ilondanga <stilondanga@gmail.com>
2018-12-13 08:19:22 +0300

    add css/styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,37 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+// Default styles
+document.getElementsByTagName("html")[0].className = "pink";
+document.getElementById("title").className = "pink";
+document.getElementsByTagName("fieldset")[0].className = "pink";
+var inputArray = document.getElementsByTagName("input");
+for (var i = 0; i < inputArray.length; i++) {
+  document.getElementsByTagName("input")[i].className = "pink";
+}
+document.getElementById("submit").className = "pink";
+
+// Select event
+document.getElementById("fav_color").onchange = function() {
+  updateColors()
+};
+
+function updateColors() {
+  var x = document.getElementById("fav_color");
+  var xcolor = "pink";
+  if (x.value != 'Select color...') {
+    var xcolor = x.value.toLowerCase();
+  }
+  // updating
+  console.log(x.value, xcolor);
+  document.getElementsByTagName("html")[0].className = xcolor;
+  document.getElementById("title").className = xcolor;
+  document.getElementsByTagName("fieldset")[0].className = xcolor;
+  var inputArray = document.getElementsByTagName("input");
+  for (var i = 0; i < inputArray.length; i++) {
+    document.getElementsByTagName("input")[i].className = xcolor;
+  }
+  document.getElementById("submit").className = xcolor;
+
+}
